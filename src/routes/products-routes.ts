d77a1a18399844d5cd233f94650f53ccd3a38c88
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-31 21:16:24 -0300

    create first structure - products

---
+++ src/routes/products-routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { ProductController } from "../controllers/products-controller";
+
+const productsRoutes = Router();
+const productsController = new ProductController();
+productsRoutes.get("/", productsController.index);
+
+export { productsRoutes };
