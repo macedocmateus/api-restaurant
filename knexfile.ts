63884537874b7da95197ddbb9d515761fd86a072
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-02 19:32:51 -0300

    create pool to restrition between foreign keys

--- knexfile.ts
+++ knexfile.ts
@@ -3,6 +3,15 @@ export default {
     connection: {
         filename: "./src/database/database.db",
     },
+
+    // Criando pool para restrição entre as chaves estrangeiras entre as tabelas, para não permitir que adicione dentro de um pedido um produto que não exista.
+    pool: {
+        afterCreate: (connection: any, done: any) => {
+            connection.run("PRAGMA foreign_keys = ON");
+            done();
+        },
+    },
+
     useNullAsDefault: true,
     migrations: {
         extensions: "ts",
