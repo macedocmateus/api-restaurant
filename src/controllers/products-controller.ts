93bcc97f120d3cef6617b2cdece89f63d58ac4ae
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-03 11:06:28 -0300

    validating with zod the name and price from products table

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,30 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "../utils/AppError";
+import { z } from "zod";
 
 class ProductController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
             return response.json({ message: "ok" });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async create(request: Request, response: Response, next: NextFunction) {
+        try {
+            const bodySchema = z.object({
+                name: z
+                    .string({ required_error: "Name is required!" })
+                    .trim()
+                    .min(6),
+                price: z
+                    .number({ required_error: "Price is required!" })
+                    .gt(0, { message: "Value must be greater than 0" }),
+            });
+
+            const { name, price } = bodySchema.parse(request.body);
+
+            return response.status(201).json({ name, price });
         } catch (error) {
             next(error);
         }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -3,6 +3,9 @@ import { ProductController } from "../controllers/products-controller";
 
 const productsRoutes = Router();
 const productsController = new ProductController();
+
 productsRoutes.get("/", productsController.index);
+
+productsRoutes.post("/", productsController.create);
 
 export { productsRoutes };
