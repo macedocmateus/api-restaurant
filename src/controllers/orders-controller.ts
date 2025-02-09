cbd8051c0dd2743d5619b9bf3694448bb85b5736
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-09 07:49:11 -0300

    add treatment of products and table sessions with zod for data validation

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -12,12 +12,39 @@ class OrdersController {
                 quantity: z.number(),
             });
 
-            // Receber os dados
+            // Recebendo os dados
             const { table_session_id, product_id, quantity } = bodySchema.parse(
                 request.body,
             );
 
-            return response.status(201).json();
+            // Tratando as exceções do table session (sessão da mesa) na tabela de sessões das mesas
+            const session = await knex<TablesSessionsRepository>(
+                "tables_sessions",
+            )
+                .where({ id: table_session_id })
+                .first();
+
+            // Verifica se a sessão existe
+            if (!session) {
+                throw new AppError("session table not found");
+            }
+
+            // Verifica se a sessão está fechada
+            if (session.closed_at) {
+                throw new AppError("this table is closed");
+            }
+
+            // Tratando as exceções do product (produto) na tabela de produtos
+            const product = await knex<ProductRepository>("products")
+                .where({ id: product_id })
+                .first();
+
+            // Verifica se o produto existe
+            if (!product) {
+                throw new AppError("product not found");
+            }
+
+            return response.status(201).json(product);
         } catch (error) {
             next(error);
         }
